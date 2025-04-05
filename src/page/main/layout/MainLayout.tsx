c286796bf60064e7075529f441992a3511fb1744
Achintha Isuru <achinthaisuru444@gmail.com>
2025-04-05 13:32:04 -0400

    chore: install tailwind

--- src/page/main/layout/MainLayout.tsx
+++ src/page/main/layout/MainLayout.tsx
@@ -2,7 +2,7 @@ import Header from "../components/Header";
 
 function MainLayout() {
     return (
-        <div className="h-screen bg-gray-100">
+        <div className="h-screen w-screen bg-red-100">
             <Header />
         </div>
     );
